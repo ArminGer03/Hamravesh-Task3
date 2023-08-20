16465947b9a345e06ed7f669c4972007c2d3d1c6
ArminGer03 <a1382.geramirad@gmail.com>
2023-08-20 11:59:51 +0330

    renamed python file

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 from flask import Flask, render_template, request, session, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
-
 app = Flask(__name__)
 app.secret_key = 'felan-alaki-yechizi-mizaram'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 db = SQLAlchemy(app)
 
 class User(db.Model):
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
     password = db.Column(db.String(50), nullable=False)
@@ -19,15 +19,26 @@ def home():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        # Check login credentials and set session variables
-        session['username'] = request.form['username']
-        return redirect(url_for('dashboard'))
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+
+        if user:
+            if user.password == password:
+                session['username'] = username
+                return redirect(url_for('dashboard'))
+            else:
+                error = 'Invalid password. Please try again.'
+                return render_template('login.html', error=error)
+        else:
+            error = 'Username does not exist. Please sign up.'
+            return render_template('login.html', error=error)
+
     return render_template('login.html')
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
     if request.method == 'POST':
-        # Create a new user account and set session variables
         username = request.form['username']
         password = request.form['password']
         new_user = User(username=username, password=password)
@@ -36,7 +47,6 @@ def signup():
         session['username'] = username
         return redirect(url_for('dashboard'))
     return render_template('signup.html')
-
 
 @app.route('/dashboard')
 def dashboard():
@@ -50,4 +60,4 @@ def logout():
     return redirect(url_for('login'))
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
