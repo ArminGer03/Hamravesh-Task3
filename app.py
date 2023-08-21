a19e4bad745abe96abb06c64931e22361ca940b8
ArminGer03 <a1382.geramirad@gmail.com>
2023-08-21 13:04:24 +0330

    Added history database

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ import random
 app = Flask(__name__)
 app.secret_key = 'felan-alaki-yechizi-mizaram'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SQLALCHEMY_BINDS'] = {
+    'histories': 'sqlite:///histories.db'
+}
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 class User(db.Model):
@@ -14,6 +18,14 @@ class User(db.Model):
     username = db.Column(db.String(50), unique=True, nullable=False)
     password = db.Column(db.String(50), nullable=False)
 
+class History(db.Model):
+    __bind_key__ = 'histories'
+    __tablename__ = 'history'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), nullable=False)
+    min_bound = db.Column(db.Integer, nullable=False)
+    max_bound = db.Column(db.Integer, nullable=False)
+    random_number = db.Column(db.Integer, nullable=False)
 
 def check_password_format(password):
     # Check for whitespace
@@ -162,6 +174,9 @@ def dashboard():
             return render_template('dashboard.html',user_in_session=session['username'] , error=error, random_number=random_number)
         else:
             random_number = random.randint(min, max)
+            new_history = History(username=session['username'], min_bound=min, max_bound=max, random_number=random_number)
+            db.session.add(new_history)
+            db.session.commit()
             return render_template('dashboard.html',user_in_session=session['username'] , error=error, random_number=random_number)
 
     return render_template('dashboard.html', user_in_session=session['username'], username=session['username'])
